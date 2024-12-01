61ac5ca4745fc02ab2504046198d4aed5495704e
nataliyatraxler <nataliya.traxler@gmail.com>
2024-12-01 16:13:04 +0100

    hw1

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,50 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ProfileApp from './components/Profile/App.jsx';
 import FriendListApp from './components/FriendList/App.jsx';
 import TransactionHistoryApp from './components/TransactionsHistory/App.jsx';
+import './App.css';
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <nav>
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Profile
+        </NavLink>
+        <NavLink
+          to="/friendslist"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Friends List
+        </NavLink>
+        <NavLink
+          to="/transactions"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Transactions
+        </NavLink>
+      </nav>
 
-const APP_TO_RENDER = import.meta.env.VITE_APP_TO_RENDER;
-console.log('VITE_APP_TO_RENDER:', import.meta.env.VITE_APP_TO_RENDER);
-console.log('APP_TO_RENDER:', APP_TO_RENDER);
-
-let App;
-if (APP_TO_RENDER === 'profile') {
-  App = ProfileApp;
-} else if (APP_TO_RENDER === 'friendslist') {
-  App = FriendListApp;
-} else if (APP_TO_RENDER === 'transactions') {
-  App = TransactionHistoryApp; 
-} else {
-  throw new Error('APP_TO_RENDER is not specified or incorrect!');
-}
+      <Routes>
+        <Route path="/profile" element={<ProfileApp />} />
+        <Route path="/friendslist" element={<FriendListApp />} />
+        <Route path="/transactions" element={<TransactionHistoryApp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
